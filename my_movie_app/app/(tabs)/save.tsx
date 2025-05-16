a82a683b3416394a91af54814713d1a76ace7479
brandon browm <brandonb77706@gmail.com>
2025-05-15 21:00:11 -0400

    completed routing and navigation for 4 pages. add icons and new asset files

--- my_movie_app/app/(tabs)/save.tsx
+++ my_movie_app/app/(tabs)/save.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const saved = () => {
+const save = () => {
   return (
     <View>
-      <Text>saved</Text>
+      <Text>save</Text>
     </View>
   )
 }
 
-export default saved
+export default save
