a98e576832644429115e156811a5204c0c36c66e
brandon browm <brandonb77706@gmail.com>
2025-05-25 20:33:07 -0400

    Add movie listing with API integration and new UI components
    Introduces a movie listing feature using API integration. Implements a `FlatList` to display movies fetched via a custom hook and adds error/loading states for improved user experience.
    
    Creates a reusable `MovieCard` component to display movie details. Adjusts Tailwind configuration for expanded styling options. Enhances the app's usability and visual appeal.

--- my_movie_app/tailwind.config.js
+++ my_movie_app/tailwind.config.js
@@ -1,26 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default  {
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets :[require("nativewind/preset")],
+export default {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./componets/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       colors: {
         primary: "#030014",
-        secondary: '#151312',
-        light:{
-          100: '#D6C6FF',
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
           200: "#A8B5DB",
           300: "#9CA4AB",
         },
-        dark : {
-          100 : '#221f3d',
-          200:'#0f0d23',
+        dark: {
+          100: "#221f3d",
+          200: "#0f0d23",
         },
         accent: "#AB8BFF",
-        
-      }
+      },
     },
   },
   plugins: [],
-}
+};
 
